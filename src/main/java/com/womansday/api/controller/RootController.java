8601c67447c514f2b2ff69c6e243774fc631411e
Aleksandr Pavlenko <a.pavlenko@lichi.me>
2026-03-04 15:33:14 +0300

    add /api

--- src/main/java/com/womansday/api/controller/RootController.java
+++ src/main/java/com/womansday/api/controller/RootController.java
@@ -2,6 +2,7 @@ package com.womansday.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.womansday.api.exception.TeapotException;
@@ -9,6 +10,7 @@ import com.womansday.api.exception.TeapotException;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class RootController {
 
     @GetMapping("/")
