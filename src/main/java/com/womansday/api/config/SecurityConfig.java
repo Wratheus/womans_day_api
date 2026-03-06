7d94e07ed56262ad0420ce5107b18466c766cc82
Aleksandr Pavlenko <a.pavlenko@lichi.me>
2026-03-07 02:29:43 +0300

    rm

--- src/main/java/com/womansday/api/config/SecurityConfig.java
+++ src/main/java/com/womansday/api/config/SecurityConfig.java
@@ -56,7 +56,6 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/change-password").authenticated()
                         .requestMatchers("/api/auth/**").permitAll()
                         // .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/api/lootbox/purchase").denyAll()
                         .requestMatchers("/api/admin/users/*/bonus").denyAll()
                         .requestMatchers("/api/admin/**").hasRole(Role.ADMIN.name())
                         .anyRequest().authenticated())
